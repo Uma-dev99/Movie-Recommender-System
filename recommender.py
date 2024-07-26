58d6a197f9914af8f1205b526a86bdf9c81dbb47
Uma Shankar Singh <83299139+Uma-dev99@users.noreply.github.com>
2024-07-26 15:07:22 +0530

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -19,3 +19,28 @@ als = ALS(maxIter=10,regParam=0.16,userCol="userId",ratingCol="rating",itemCol="
 model = als.fit(train)
 
 pred = model.transform(test)
+
+
+eva = RegressionEvaluator(metricName="rmse",labelCol="rating",predictionCol="prediction")
+rmse = eva.evaluate(pred.where("prediction != 'NaN'"))
+
+print("RMSE score is : {}".format(rmse))
+    
+user_1 = test.filter(test["userId"]==1).select(["movieId","userId"])
+recommend = model.transform(user_1)
+pref = recommend.orderBy("prediction",ascending=False)
+
+# Top 5 recommendations for USER 1
+pref_pandas = pref.limit(5).toPandas()
+movie_ids = list(pref_pandas.movieId)
+pred_ratings = list(pref_pandas.movieId)
+
+df_movies = spark.read.csv(r'C:\Users\MSI\Desktop\ds project\movies.csv',header=True,inferSchema=True)
+
+movies = []
+for i in movie_ids:
+    movies.append(df_movies.filter("movieId = {}".format(i)).toPandas().title[0])
+    
+dic = {'movieID':movie_ids,'movieName':movies, 'predictedRating':pref_pandas.prediction}
+output = pd.DataFrame(dic)
+print(output)
